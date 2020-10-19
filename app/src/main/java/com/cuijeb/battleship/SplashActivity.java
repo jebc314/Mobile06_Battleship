658e741e3976554baec26a3c72f3d5bcb2695b83
jebc314 <jebcui99@gmail.com>
2020-10-19 12:01:50 -0400

    Edits: Added splash activity and also added a gif to splash activity. Changed app icon.

---
+++ app/src/main/java/com/cuijeb/battleship/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.cuijeb.battleship;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Timer to wait before showing enemy attack
+        new CountDownTimer(3500, 1000) {
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            public void onTick(long millisUntilFinished) {
+                // millisUntilFinished    The amount of time until finished.
+            }
+        }.start();
+    }
+}
